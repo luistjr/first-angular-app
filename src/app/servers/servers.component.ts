a682edd1d36550bdff836126ecdfd1e8ca5ea586
therunningagent <luistjr@gmail.com>
2021-07-22 10:50:38 -0700

    continued directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false; 
+  serverCreationStatus = 'No server was created';
+  serverName ='';
+  serverCreated = false;
 
   constructor() { 
     setTimeout(() => {
@@ -16,6 +19,15 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
